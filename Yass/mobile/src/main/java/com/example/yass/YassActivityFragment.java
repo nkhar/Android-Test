6697df0b23a6081836797e99d841a7b295ca9778
nkhar <Nikoloz.Kharshiladze.2014@live.rhul.ac.uk>
2016-10-24 22:28:41 +0100

    Adding yass android app

---
+++ Yass/mobile/src/main/java/com/example/yass/YassActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.yass;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class YassActivityFragment extends Fragment {
+
+    public YassActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_yass, container, false);
+    }
+}
